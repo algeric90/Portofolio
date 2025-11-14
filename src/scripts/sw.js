ab466180047953e5e946b92d8b5b1cab6a712102
sega <sshunter2013@gmail.com>
2025-11-14 22:17:31 +0700

    fixing bug on shifting layout on load

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -7,40 +7,6 @@ import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 // Precache all built assets
 precacheAndRoute(self.__WB_MANIFEST);
 
-// Google Fonts
-registerRoute(
-  ({ url }) =>
-    url.origin === 'https://fonts.googleapis.com' ||
-    url.origin === 'https://fonts.gstatic.com',
-  new CacheFirst({
-    cacheName: 'google-fonts',
-    plugins: [
-      new CacheableResponsePlugin({ statuses: [0, 200] }),
-      new ExpirationPlugin({
-        maxEntries: 30, // simpan max 30 file font
-        maxAgeSeconds: 60 * 60 * 24 * 365, // simpan selama 1 tahun
-      }),
-    ],
-  }),
-);
-
-// Fontawesome CDN
-registerRoute(
-  ({ url }) =>
-    url.origin === 'https://cdnjs.cloudflare.com' ||
-    url.origin.includes('fontawesome'),
-  new CacheFirst({
-    cacheName: 'fontawesome',
-    plugins: [
-      new CacheableResponsePlugin({ statuses: [0, 200] }),
-      new ExpirationPlugin({
-        maxEntries: 20,
-        maxAgeSeconds: 60 * 60 * 24 * 180, // 6 bulan
-      }),
-    ],
-  }),
-);
-
 // Gambar & assets lokal
 registerRoute(
   ({ request }) => request.destination === 'image',
