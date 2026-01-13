1d61e836163476ed199002b6db6701d1b0660814
sega <sshunter2013@gmail.com>
2026-01-13 18:33:06 +0700

    chore: setup bootstrap 5

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import "./scss/main.scss"
 import App from './App.jsx'
+import * as bootstrap from "bootstrap";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
